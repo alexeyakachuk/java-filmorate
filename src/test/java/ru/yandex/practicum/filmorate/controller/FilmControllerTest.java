077bf869853c9bbaf5e5ed6c8007a1cf582f9948
Username <box03-853@yandex.ru>
2024-06-12 02:07:56 +0400

    Задача 10 спринта. Убрал импорт

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.*;
 
